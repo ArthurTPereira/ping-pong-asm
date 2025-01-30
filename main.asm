5143fdadb5e6e3b07074853da986c1a2d09f3932
ArthurTPereira <arthur.t.pereira@edu.ufes.br>
2025-01-30 18:09:43 -0300

    feature: interrupção INT 9h

--- main.asm
+++ main.asm
@@ -15,6 +15,45 @@ segment code
     	MOV 	SS,AX
     	MOV 	SP,stacktop
 
+		CLI								; Deshabilita INTerrupções por hardware - pin INTR NÃO atende INTerrupções externas	
+        XOR     AX, AX					; Limpa o registrador AX, é equivalente a fazer "MOV AX,0"
+        MOV     ES, AX					; Inicializa o registrador de Segmento Extra ES para acessar à região de vetores de INTerrupção (posição zero de memoria)
+        MOV     AX, [ES:INT9*4]			; Carrega em AX o valor do IP do vector de INTerrupção 9 
+        MOV     [offset_dos], AX    	; Salva na variável offset_dos o valor do IP do vector de INTerrupção 9
+        MOV     AX, [ES:INT9*4+2]   	; Carrega em AX o valor do CS do vector de INTerrupção 9
+        MOV     [cs_dos], AX			; Salva na variável cs_dos o valor do CS do vector de INTerrupção 9     
+        MOV     [ES:INT9*4+2], CS		; Atualiza o valor do CS do vector de INTerrupção 9 com o CS do programa atual 
+        MOV     WORD [ES:INT9*4],keyINT	; Atualiza o valor do IP do vector de INTerrupção 9 com o offset "keyINT" do programa atual
+        STI	
+
+		JMP modo_video
+
+
+keyINT:									; Este segmento de código só será executado se uma tecla for presionada, ou seja, se a INT 9h for acionada!
+        PUSH    AX						; Salva contexto na pilha
+        PUSH    BX
+        PUSH    DS
+ ;       MOV     AX,data					; Carrega em AX o endereço de "data" -> Região do código onde encontra-se o segemeto de dados "Segment data" 			
+ ;       MOV     DS,AX					; Atualiza registrador de segmento de dados DS, isso pode ser feito no inicio do programa!
+        IN      AL, kb_data				; Le a porta 60h, que é onde está o byte do Make/Break da tecla. Esse valor é fornecido pelo chip "8255 PPI"
+        INC     WORD [p_i]				; Incrementa p_i para indicar no loop principal que uma tecla foi acionada!
+        AND     WORD [p_i],7			
+        MOV     BX,[p_i]				; Carrega p_i em BX
+        MOV     [BX+tecla],al			; Transfere o valor Make/Break da tecla armacenado em AL "linha 84" para o segmento de dados com offset DX, na variável "tecla"
+        IN      AL, kb_ctl				; Le porta 61h, pois o bit mais significativo "bit 7" 
+        OR      AL, 80h					; Faz operação lógica OR com o bit mais significativo do registrador AL (1XXXXXXX) -> Valor lido da porta 61h 
+        OUT     kb_ctl, AL				; Seta o bit mais significativo da porta 61h
+        AND     AL, 7Fh					; Restablece o valor do bit mais significativo do registrador AL (0XXXXXXX), alterado na linha 90 	
+        OUT     kb_ctl, AL				; Reinicia o registrador de dislocamento 74LS322 e Livera a interrupção "CLR do flip-flop 7474". O 8255 - Programmable Peripheral Interface (PPI) fica pronto para recever um outro código da tecla https://es.wikipedia.org/wiki/INTel_8255
+        MOV     AL, eoi					; Carrega o AL com a byte de End of Interruption, -> 20h por default
+        OUT     pictrl, AL				; Livera o PIC
+        
+		POP     DS						; Reestablece os registradores salvos na linha 79 
+        POP     BX
+        POP     AX
+        IRET							; Retorna da interrupção
+
+modo_video:
 ;Salvar modo corrente de video(vendo como esta o modo de video da maquina)
         MOV  	AH,0Fh
     	INT  	10h
@@ -141,35 +180,29 @@ l_dificil:
 		MOV 	byte[dificuldade], 0
 
 selection_loop:
-		CALL    check_key
-		CMP     AL, 0
-		JE      selection_loop
-		JMP    handle_key
-
-
-check_key:
-		MOV    AH, 01h
-		INT    16h
-		JZ     no_key
-		MOV    AH, 00h
-		INT    16h
-		RET
-
-
-no_key:
-		MOV    AL, 0
-		RET
+        MOV     AX,[p_i]				
+		CMP     AX,[p_t]
+        JE      selection_loop
+		JMP     handle_key
 
 handle_key:
-		CMP    AL, 61h
+		INC     word[p_t]				; p_t - se atualiza (p_t = p_t + 1) só se p_i foi atualizado, ou seja, se teve tecla pulsada
+        AND     word[p_t],7				
+        MOV     BX,[p_t]				; Carrega em BX o valor de p_t
+        XOR     AX, AX
+        MOV     AL, [BX+tecla]			; Carrega em AL o valor da variável tecla (variável atualizada durante a ISR) mais o offset BX, AL <- [BX+tecla]  
+        MOV     [tecla_u],AL
+
+		
+		CMP    BYTE [tecla_u], 1Eh
 		JE     selection_left
-		CMP    AL, 64h
+		CMP    BYTE [tecla_u], 20h
 		JE     selection_right
 
-		CMP	   AL, 1Bh
-		JE	   selection_quit
+		CMP	   BYTE [tecla_u], 10h
+		JE	   near fechar_jogo
 
-		CMP    AL, 0Dh
+		CMP    BYTE [tecla_u], 1Ch
 		JE    selection_enter
 		JMP     selection_loop
 
@@ -209,13 +242,6 @@ inicia_jogo:
 		CALL	reset_tela
 		JMP		cria_campo
 	    
-
-selection_quit:
-		MOV  	AH,0   						; set video mode
-	    MOV  	AL,[modo_anterior]   		; modo anterior
-	    INT  	10h
-		MOV     AX,4c00h
-		INT     21h
 
 
 erase_cursor:
@@ -596,28 +622,27 @@ atualiza_raquete_direita_baixo:
 	CALL desenha_raquete_direita
 	JMP near continuacao_raquete
 
-check_key_jogo:
-		MOV    AH, 01h
-		INT    16h
-		JZ     near continuacao_raquete
-		MOV    AH, 00h
-		INT    16h
-		RET
-
 handle_key_jogo:
-	CMP	   AL, 77h
+	INC     word[p_t]				; p_t - se atualiza (p_t = p_t + 1) só se p_i foi atualizado, ou seja, se teve tecla pulsada
+	AND     word[p_t],7				
+	MOV     BX,[p_t]				; Carrega em BX o valor de p_t
+	XOR     AX, AX
+	MOV     AL, [BX+tecla]			; Carrega em AL o valor da variável tecla (variável atualizada durante a ISR) mais o offset BX, AL <- [BX+tecla]  
+	MOV     [tecla_u],AL
+
+	CMP	   byte[tecla_u], 11h
 	JE 	   near atualiza_raquete_esquerda_cima
-	CMP    AL, 73h
+	CMP    byte[tecla_u], 1Fh
 	JE 	   near atualiza_raquete_esquerda_baixo
-	CMP    AL, 6Fh
+	CMP    byte[tecla_u], 48h
 	JE     atualiza_raquete_direita_cima
-	CMP    AL, 6Ch
+	CMP    byte[tecla_u], 50h
 	JE	   atualiza_raquete_direita_baixo
 	
-	CMP    AL, 71h
+	CMP    AL, 10h
 	JE     sair
 
-	CMP	   AL, 70h
+	CMP	   AL, 19h
 	JE 	   key_pause
 
 	JMP    near continuacao_raquete
@@ -639,21 +664,21 @@ l_pause:
 		; INC		byte[cor]				;mudar a cor para a seguinte
     	LOOP    l_pause
 pausa:
-	CALL    check_key_pause
-	CMP     AL, 0
-	JE      pausa
-	JMP     handle_key_pause
-
-check_key_pause:
-		MOV    AH, 01h
-		INT    16h
-		JZ   near no_key
-		MOV    AH, 00h
-		INT    16h
-		RET
+        MOV     AX,[p_i]				
+		CMP     AX,[p_t]
+        JE      pausa
+		JMP     handle_key_pause
 
 handle_key_pause:
-	CMP	   AL, 70h
+
+	INC     word[p_t]				; p_t - se atualiza (p_t = p_t + 1) só se p_i foi atualizado, ou seja, se teve tecla pulsada
+	AND     word[p_t],7				
+	MOV     BX,[p_t]				; Carrega em BX o valor de p_t
+	XOR     AX, AX
+	MOV     AL, [BX+tecla]			; Carrega em AL o valor da variável tecla (variável atualizada durante a ISR) mais o offset BX, AL <- [BX+tecla]  
+	MOV     [tecla_u],AL
+
+	CMP	   AL, 19h
 	JE 	   resume
 	JMP    near pausa
 
@@ -707,31 +732,26 @@ l_sair:
 		CALL 	line
 
 exit_loop:
-		CALL    check_key_exit_loop
-		CMP     AL, 0
-		JE     near exit_loop
-		JMP    handle_key_exit_loop
-
-check_key_exit_loop:
-		MOV    AH, 01h
-		INT    16h
-		JZ     no_key_exit_loop
-		MOV    AH, 00h
-		INT    16h
-		RET
-
-
-no_key_exit_loop:
-		MOV    AL, 0
-		RET
+	MOV     AX,[p_i]				
+	CMP     AX,[p_t]
+	JE      exit_loop
+	JMP     handle_key_exit_loop
 
 handle_key_exit_loop:
-		CMP    AL, 61h
+
+	INC     word[p_t]				; p_t - se atualiza (p_t = p_t + 1) só se p_i foi atualizado, ou seja, se teve tecla pulsada
+	AND     word[p_t],7				
+	MOV     BX,[p_t]				; Carrega em BX o valor de p_t
+	XOR     AX, AX
+	MOV     AL, [BX+tecla]			; Carrega em AL o valor da variável tecla (variável atualizada durante a ISR) mais o offset BX, AL <- [BX+tecla]  
+	MOV     [tecla_u],AL
+
+		CMP    AL, 1Eh
 		JE     selection_left_exit_loop
-		CMP    AL, 64h
+		CMP    AL, 20h
 		JE     selection_right_exit_loop
 
-		CMP    AL, 0Dh
+		CMP    AL, 1Ch
 		JE    near selection_enter_exit_loop
 		JMP     exit_loop
 
@@ -863,14 +883,22 @@ fechar_jogo:
 	MOV  	AH,0   						; set video mode
 	MOV  	AL,[modo_anterior]   		; modo anterior
 	INT  	10h
-	MOV     AX,4c00h
-	INT     21h
+
+	CLI								; Deshabilita INTerrupções por hardware - pin INTR NÃO atende INTerrupções externas
+	XOR     AX, AX					; Limpa o registrador AX, é equivalente a fazer "MOV AX,0"				
+	MOV     ES, AX					; Inicializa o registrador de Segmento Extra ES para acessar à região de vetores de INTerrupção (posição zero de memoria)
+	MOV     AX, [cs_dos]			; Carrega em AX o valor do CS do vector de INTerrupção 9 que foi salvo na variável cs_dos -> linha 25
+	MOV     [ES:INT9*4+2], AX		; Atualiza o valor do CS do vector de INTerrupção 9 que foi salvo na variável cs_dos
+	MOV     AX, [offset_dos]		; Carrega em AX o valor do IP do vector de INTerrupção 9 que foi salvo na variável offset_dos -> linha 23
+	MOV     [ES:INT9*4], AX 		; Atualiza o valor do IP do vector de INTerrupção 9 que foi salvo na variável offset_dos
+	MOV     AH, 4Ch					; Carrega em AH o valor de 4Ch, parametro para INT 21h
+	INT     21h						; Chama Interrupção 21h para retornar o controle ao sistema operacional -> sai de forma segura da execução do programa
+
 
 
 loop_jogo:
-
-	CALL    check_key_jogo
-	CMP     AL, 0
+	MOV     AX,[p_i]				
+	CMP     AX,[p_t]
 	JE      continuacao_raquete
 	JMP     handle_key_jogo
 
@@ -1244,31 +1272,26 @@ rejogar:
 		JMP paint_yes_selected
 
 yes_no_loop:
-		CALL    check_key_yes_no
-		CMP     AL, 0
-		JE      yes_no_loop
-		JMP    handle_key_yes_no
-
-check_key_yes_no:
-		MOV    AH, 01h
-		INT    16h
-		JZ     no_key_yes_no
-		MOV    AH, 00h
-		INT    16h
-		RET
-
-
-no_key_yes_no:
-		MOV    AL, 0
-		RET
+        MOV     AX,[p_i]				
+		CMP     AX,[p_t]
+        JE      yes_no_loop
+		JMP     handle_key_yes_no
 
 handle_key_yes_no:
-		CMP    AL, 61h
+
+		INC     word[p_t]				; p_t - se atualiza (p_t = p_t + 1) só se p_i foi atualizado, ou seja, se teve tecla pulsada
+        AND     word[p_t],7				
+        MOV     BX,[p_t]				; Carrega em BX o valor de p_t
+        XOR     AX, AX
+        MOV     AL, [BX+tecla]			; Carrega em AL o valor da variável tecla (variável atualizada durante a ISR) mais o offset BX, AL <- [BX+tecla]  
+        MOV     [tecla_u],AL
+
+		CMP    byte[tecla_u], 1Eh
 		JE     selection_left_yes_no
-		CMP    AL, 64h
+		CMP    byte[tecla_u], 20h
 		JE     selection_right_yes_no
 
-		CMP    AL, 0Dh
+		CMP    byte[tecla_u], 1Ch
 		JE    near selection_enter_yes_no
 		JMP     yes_no_loop
 
@@ -1541,7 +1564,18 @@ verde_direito dw 0
 azul_claro_direito dw 0
 azul_escuro_direito dw 0
 
-
+kb_data EQU 60h  				; PORTA DE LEITURA DE TECLADO
+kb_ctl  EQU 61h  				; PORTA DE RESET PARA PEDIR NOVA INTERRUPCAO
+pictrl  EQU 20h					; PORTA DO PIC DE TECLADO
+eoi     EQU 20h					; Byte de final de interrupção PIC - resgistrador
+INT9    EQU 9h					; Interrupção por hardware do teclado
+cs_dos  DW  1					; Variável de 2 bytes para armacenar o CS da INT 9
+offset_dos  DW 1				; Variável de 2 bytes para armacenar o IP da INT 9
+tecla_u db 0
+tecla   resb  8					; Variável de 8 bytes para armacenar a tecla presionada. Só precisa de 2 bytes!	 
+p_i     dw  0   				; Indice p/ Interrupcao (Incrementa na ISR quando pressiona/solta qualquer tecla)  
+p_t     dw  0   				; Indice p/ Interrupcao (Incrementa após retornar da ISR quando pressiona/solta qualquer tecla)    
+teclasc DB  0,0,13,10,'$'		; Variável tipo char para printar o código Make/Break em hexadecimal
 
 
 ;*************************************************************************
